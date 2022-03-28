7a801218149fe7c355da70e4bf60de8f5689dbae
Aidan Border <planes4u@outlook.com>
2022-03-28 13:04:57 -0400

    This is a disaster. I'm so sorry

--- src/Main.java
+++ src/Main.java
@@ -1,78 +1,108 @@
 /*Aidan Border
-* Module 4 Assignment 4
-* Implementing a depth-first search algorithm */
+ * Module 4 Assignment 4
+ * Implementing a depth-first search algorithm */
 
 /*ALGORITHM:
-* DFS(G)
-* Implements a depth-first search traversal of a given graph
-* Input: Graph G = {V, E}
-* Output: Graph G with its vertices marked with consecutive integers
-* In the order they've been first encountered by the DFS traversal mark each vertex in V with 0 as a mark of being
-* "unvisited"
-* count <- 0
-* for each vertex in V do
-*   if v is marked with 0
-*       dfs(v)
-* print the order in which vertices are visited.
-*
-* dfs(v)
-* Visits recursively all the unvisited vertices connected to vertex v by a path and numbers them in the order they
-* are encountered.
-* via global variable count
-* count <- count + 1; mark v with count
-*
-* for each vertex w in V adjacent to v do
-*   if w is marked with 0
-*       dfs(w)*/
+ * DFS(G)
+ * Implements a depth-first search traversal of a given graph
+ * Input: Graph G = {V, E}
+ * Output: Graph G with its vertices marked with consecutive integers
+ * In the order they've been first encountered by the DFS traversal mark each vertex in V with 0 as a mark of being
+ * "unvisited"
+ * count <- 0
+ * for each vertex in V do
+ *   if v is marked with 0
+ *       dfs(v)
+ * print the order in which vertices are visited.
+ *
+ * dfs(v)
+ * Visits recursively all the unvisited vertices connected to vertex v by a path and numbers them in the order they
+ * are encountered.
+ * via global variable count
+ * count <- count + 1; mark v with count
+ *
+ * for each vertex w in V adjacent to v do
+ *   if w is marked with 0
+ *       dfs(w)*/
+
+import java.util.Arrays;
 
 public class Main {
-    static int count = 0;
+	static int count = 0;
 
-    /*main()
-    * Params: String[] args
-    * Returns N/A (main does not return)
-    * Description: Method to support running/testing of program code
-    * Special: main() is called by java at runtime and only exits on program exit*/
-    public static void main(String[] args) {
+	/*main()
+	 * Params: String[] args
+	 * Returns N/A (main does not return)
+	 * Description: Method to support running/testing of program code
+	 * Special: main() is called by java at runtime and only exits on program exit*/
+	public static void main(String[] args) {
 
-        char[] vertArray = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'};
-        String[] edgeArray = {"ab", "ac", "ad", "bc", "bd", "cd", "de", "eh", "ef", "hg", "hf", "fg"};
-        byte[][] graf = generateGraph(vertArray, edgeArray);
+		char[] vertArray = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'};
+		String[] edgeArray = {"ab", "ac", "ad", "bc", "bd", "de", "eh", "ef", "hg", "hf", "fg"};
+		byte[][] graf = generateGraph(vertArray, edgeArray);
+		int[] visitedArray = new int[vertArray.length];
+		Arrays.fill(visitedArray, 0);
 
-        for (int i = 0; i < graf.length; i++) {
-            
-        }
+		String visited = "";
 
-    }
+		for (int i = 0; i < vertArray.length; i++) {
+			if (visitedArray[i] == 0) {
+				visited = dfs(vertArray[i], graf, vertArray, visitedArray, visited);
+			}
+		}
 
-    /*dfs()
-    * Parameters: Graph g
-    * Returns: N/A (void) for now
-    * Description: Recursively visits all unvisited vertices connected to a vertex v by a valid path and numbers them
-    *  in the order they're encountered
-    * Special: Method is recursive*/
-    public static void dfs(byte[][] graf, char[] vertArray, String[] edgeArray) {
-        count += 1;
-        for (String edge : edgeArray) {
+//		for (int i = 0; i < visitedArray.length; i++) {
+//			System.out.println(vertArray[visitedArray[i]]);
+//		}
 
-        }
+		String result = new StringBuffer(visited).reverse().toString();
+		System.out.println(result);
 
-    }
+	}
 
-    /*generateGraph()
-     * Parameters: char[] vertices, String[] edges
-     * Returns: btye[][]
-     * Description: Generate us a graph*/
-    public static byte[][] generateGraph(char[] vertices, String[] edges){
-        byte[][] graph = new byte[vertices.length][vertices.length];
-        for(String edge : edges){
-            int sourceIndex = Character.getNumericValue(edge.charAt(0)) - 10;
-            int destIndex = Character.getNumericValue(edge.charAt(1)) - 10;
-            graph[sourceIndex][destIndex] = 1;
-            graph[destIndex][sourceIndex] = 1;
-        }
-        return graph;
-    }
+	/*dfs()
+	 * Parameters: char v
+	 * Returns: N/A (void) for now
+	 * Description: Recursively visits all unvisited vertices connected to a vertex v by a valid path and numbers them
+	 *  in the order they're encountered
+	 * Special: Method is recursive*/
+	public static String dfs(char v, byte[][] graf, char[] vertArray, int[] visitedArray, String visited) {
+		count += 1;
+		int index = -1;
+
+
+		for (int i = 0; i < vertArray.length; i++) {
+			if (Character.getNumericValue(vertArray[i]) == Character.getNumericValue(v)) {
+				index = i;
+				visitedArray[i] = count;
+
+			}
+		}
+		System.out.println(visited);
+		for (int j = 0; j < graf[0].length; j++) {
+			if (visitedArray[j] == 0 && graf[index][j] == 1) {
+				visited = dfs(vertArray[j], graf, vertArray, visitedArray, visited);
+				visited = visited + vertArray[index];
+			}
+		}
+
+		return visited;
+	}
+
+	/*generateGraph()
+	 * Parameters: char[] vertices, String[] edges
+	 * Returns: btye[][]
+	 * Description: Generate us a graph*/
+	public static byte[][] generateGraph(char[] vertices, String[] edges){
+		byte[][] graph = new byte[vertices.length][vertices.length];
+		for(String edge : edges){
+			int sourceIndex = Character.getNumericValue(edge.charAt(0)) - 10;
+			int destIndex = Character.getNumericValue(edge.charAt(1)) - 10;
+			graph[sourceIndex][destIndex] = 1;
+			graph[destIndex][sourceIndex] = 1;
+		}
+		return graph;
+	}
 
 
 }
